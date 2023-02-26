ae03fe90321643d9e2901f1ff3ad6bea264e6019
YERLI-NEIRA <alfajor_@outlook.com>
2023-02-26 18:22:01 -0500

    commit

--- new-project/src/App.js
+++ new-project/src/App.js
@@ -1,17 +1,33 @@
-import './App.css';
-import { Routes, Route } from "react-router-dom";
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [parks, setParks] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://developer.nps.gov/api/v1/parks?fields=images&api_key=Gzi5YUD266c1PzJqT3jv0y1exjey9Wd7HL9uCRi8"
+    )
+      .then((response) => response.json())
+      .then((data) => setParks(data.data));
+  }, []);
+
   return (
-    <div className="App">      
-      <Routes> 
-        <Route path="/" element={<App/>}>
-        </Route>
-      </Routes>
+    <div className="App">
+      <h1>National Parks</h1>
+      <div className="park-cards">
+        {parks.map((park) => (
+          <div key={park.id} className="park-card">
+            <img src={park.images[0].url} alt={park.fullName} />
+            <h3>{park.fullName}</h3>
+            <p>{park.description}</p>
+            <button onClick={() => alert(`You clicked ${park.fullName}`)}>
+              Learn More
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
 
 export default App;
-
